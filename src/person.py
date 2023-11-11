6b829b7789837b1b114089924fb665f398605edc
Is-monp <isabellapalencia@uninorte.edu.co>
2023-11-11 16:55:59 -0500

    metodos (?

--- src/person.py
+++ src/person.py
@@ -10,9 +10,14 @@ class Jugador(Persona):
         self.__sueldo = sueldo
         self.__juegos: list["Juego"]=[]
     
+    def add_juego(self, juego: "Juego") -> None:
+        self.__juegos.append(juego)
+    
 class Juez(Persona):
     def __init__(self, nombre:str, edad:int) -> None:
         super().__init__(nombre)
         self.__edad = edad
         self.__juegos: list["Juego"]=[]
         
+    def add_juego(self, juego: "Juego") -> None:
+        self.__juegos.append(juego)

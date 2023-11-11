d2883542335fa7bc777e2cf4b1bed9b2d12250c0
Bryan <bemontoya@uninorte.edu.co>
2023-11-11 17:03:53 -0500

    Update core.py

--- src/core.py
+++ src/core.py
@@ -1,10 +1,13 @@
 from ast import List
-from person import Juez
-from person import Jugador
+from person import Juez,Jugador
 
 class Campeonato:
     def __init__(self) -> None:
         self.__juegos: List["Juego"] = []
+
+    def add_juego(self,juego:"Juego"):
+        self.__juego_siguiente = juego
+    
 
 
 
@@ -14,12 +17,21 @@ class LigaTenis:
         self.__jueces: List["Juez"] = []
         self.__jugadores: List["Jugador"] = []
 
-
     def add_juez(self,juez:Juez):
         self.__jueces.append(juez)
 
     def add_jugador(self,jugador:Jugador):
         self.__jugadores.append(jugador)
 
-    def add_juez(self,campeonato:Campeonato):
+    def add_campeonato(self,campeonato:Campeonato):
         self.__campeonatos.append(campeonato)
+
+
+    def get_jugador(self,indice):
+        return self.__jugadores[indice]
+    
+    def get_juez(self,indice):
+        return self.__jueces[indice]
+    
+    def get_campeonato(self,indice:int):
+        return self.__campeonatos[indice]
